41a9df167dd2c2c04d7b6e88b9a7e8bf1c690012
nvtdat <nvtdat30052006@gmail.com>
2026-09-19 18:18:11 +0700

    Update ca.pem relative path in database.py

--- backend/database.py
+++ backend/database.py
@@ -12,7 +12,6 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL is not set in the environment variables.")
 
-DB_SSL_CA = os.getenv("DB_SSL_CA")
 ctx = ssl.create_default_context(cafile=DB_SSL_CA) if DB_SSL_CA else None
 ctx.check_hostname = False if ctx else None
 ctx.verify_mode = ssl.CERT_NONE if ctx else None
